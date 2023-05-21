5ed218cf82d060ab0f7cdcd3fce0485c2410b853
Robert Martinez <rmartine@starbucks.com>
2023-05-20 23:24:05 -0500

    Add home.html page, set active tab on the content being displayed

--- js/script.js
+++ js/script.js
@@ -3,18 +3,27 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 // jQuery for page transitions
-$(document).ready(function() {
+$(document).ready(function () {
+  // Load initial page
+  $("#content").load("pages/home.html");
+  $(".nav-item").first().addClass("active");
+
   // Handle click events
-  $('.nav-link').click(function(e) {
+  $(".nav-link").click(function (e) {
     e.preventDefault();
 
     // Get the href attribute of the clicked link
-    var page = $(this).attr('href');
+    var page = $(this).attr("href");
 
-    $('#content').fadeOut(500, function() {
-      $('#content').load(page, function() {
-        $('#content').fadeIn(500);
+    $("#content").fadeOut(500, function () {
+      $("#content").load(page, function () {
+        $("#content").fadeIn(500);
       });
     });
+
+    // Remove active class from all nav items
+    $(".nav-item").removeClass("active");
+    // Add active class to parent nav item of the clicked link
+    $(this).parent(".nav-item").addClass("active");
   });
 });
