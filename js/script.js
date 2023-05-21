ce761706681441d9f4f8f41df20bc75a7ca8938e
Robert Martinez <rmartine@starbucks.com>
2023-05-20 22:58:56 -0500

    Add jquery transitions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
-/* script.js */
-
-// Function to add an animation class to an element
-function animate(element, animationClass) {
-  element.classList.add(animationClass);
-
-  // Remove the animation class after the animation is complete
-  element.addEventListener("animationend", function () {
-    element.classList.remove(animationClass);
-  });
-}
-
-// Animate project cards on hover
-var projectCards = document.querySelectorAll(".project-card");
-projectCards.forEach(function (card) {
-  card.addEventListener("mouseenter", function () {
-    animate(card, "bounce");
-  });
-});
-
 // Script to update the current year in the footer dynamically
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
+
+// jQuery for page transitions
+$(document).ready(function() {
+  // Handle click events
+  $('.nav-link').click(function(e) {
+    e.preventDefault();
+
+    // Get the href attribute of the clicked link
+    var page = $(this).attr('href');
+
+    $('#content').fadeOut(500, function() {
+      $('#content').load(page, function() {
+        $('#content').fadeIn(500);
+      });
+    });
+  });
+});
