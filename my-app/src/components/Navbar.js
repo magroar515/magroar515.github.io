809766e24502810330934224ca70672dce20f724
Robert Martinez <rmartine@starbucks.com>
2023-05-22 10:13:54 -0500

    Update navbar to collapse menu burger

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,24 +1,40 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link, useLocation } from "react-router-dom";
 import { Navbar as BootstrapNavbar, Nav } from "react-bootstrap";
 
 const Navbar = () => {
+  const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleSelect = () => {
+    setExpanded(false);
+  };
+
+  // Close the navbar when a link is clicked and the location changes
+  const handleLocationChange = () => {
+    setExpanded(false);
+  };
+
   return (
-    <BootstrapNavbar expand="lg">
+    <BootstrapNavbar expand="lg" expanded={expanded} onToggle={handleToggle}>
       <BootstrapNavbar.Brand as={Link} to="/">My Website</BootstrapNavbar.Brand>
       <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
       <BootstrapNavbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <NavLink className="nav-link" exact to="/">
+        <Nav className="ml-auto" activeKey={location.pathname} onSelect={handleSelect}>
+          <NavLink className="nav-link" exact to="/" onClick={handleLocationChange}>
             Home
           </NavLink>
-          <NavLink className="nav-link" to="/about">
+          <NavLink className="nav-link" to="/about" onClick={handleLocationChange}>
             About
           </NavLink>
-          <NavLink className="nav-link" to="/projects">
+          <NavLink className="nav-link" to="/projects" onClick={handleLocationChange}>
             Projects
           </NavLink>
-          <NavLink className="nav-link" to="/contact">
+          <NavLink className="nav-link" to="/contact" onClick={handleLocationChange}>
             Contact
           </NavLink>
         </Nav>
